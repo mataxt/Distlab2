d605f27e3e502e91d08813ed7f2ce165d57c76b5
Rami <rami.alsawadi1@gmail.com>
2015-10-08 10:33:10 +0200

    basicNewMVCProject

---
+++ Dist_Lab2/Dist_Lab2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dist_Lab2.MvcApplication" Language="C#" %>
---
+++ Dist_Lab2/Dist_Lab2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dist_Lab2.Startup))]
+namespace Dist_Lab2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
