afc9e7b6efee0086042de885acc78b98aca55dbe
Rami <rami.alsawadi1@gmail.com>
2015-10-09 14:26:12 +0200

    Shitz

---
+++ Dist_Lab2/Dist_Lab2/Migrations/201510081701489_start.cs
@@ -0,0 +1,40 @@
+namespace Dist_Lab2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class start : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Messages",
+                c => new
+                    {
+                        MessageId = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false, maxLength: 128),
+                        Body = c.String(nullable: false),
+                        TimeSent = c.Binary(nullable: false, fixedLength: true, timestamp: true, storeType: "rowversion"),
+                        Status = c.String(nullable: false),
+                        Sender_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.MessageId)
+                .ForeignKey("dbo.AspNetUsers", t => t.Sender_Id)
+                .Index(t => t.Sender_Id);
+            
+            AddColumn("dbo.AspNetUsers", "Message_MessageId", c => c.Int());
+            CreateIndex("dbo.AspNetUsers", "Message_MessageId");
+            AddForeignKey("dbo.AspNetUsers", "Message_MessageId", "dbo.Messages", "MessageId");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Messages", "Sender_Id", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUsers", "Message_MessageId", "dbo.Messages");
+            DropIndex("dbo.AspNetUsers", new[] { "Message_MessageId" });
+            DropIndex("dbo.Messages", new[] { "Sender_Id" });
+            DropColumn("dbo.AspNetUsers", "Message_MessageId");
+            DropTable("dbo.Messages");
+        }
+    }
+}
